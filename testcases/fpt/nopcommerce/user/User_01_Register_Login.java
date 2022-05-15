3d9d536860ef8c02fabd325c2cccaae7f7b27c4e
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-05-15 20:48:35 +0700

    Topic_01_Variable

--- testcases/fpt/nopcommerce/user/User_01_Register_Login.java
+++ testcases/fpt/nopcommerce/user/User_01_Register_Login.java
@@ -5,7 +5,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class User_01_Register_Login {
-
+	//Toàn cục
+	String homePageUrl = "";
+	
 	 @BeforeClass
 	  public void beforeClass() {
 		  
@@ -13,18 +15,28 @@ public class User_01_Register_Login {
 
 	 @Test
 	 public void TC_01() {
+		 //Cục bộ: Sử dụng trong phạm vi của testcase/ hàm
+		 String homePageUrl = ""; 
 		 
+		 System.out.println(homePageUrl);
+		 
+		 //Block code
+		 if(3<5) {
+			 //Cục bộ
+			 String homePageTitle = "";
+			 System.out.println(homePageTitle);
+		 }
 	 }
 	 
 	 @Test
 	 public void TC_02() {
-		  
+		 System.out.println(homePageUrl);
 	 }
   
  
-  @AfterClass
-  public void afterClass() {
+	 @AfterClass
+	 public void afterClass() {
 	  
-  }
+	 }
 
 }

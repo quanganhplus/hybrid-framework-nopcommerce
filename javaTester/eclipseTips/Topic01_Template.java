027aa83e622657fed1d4856051db9d6d52c63a0d
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-05-22 16:06:07 +0700

    Eclipse IDE Tips

---
+++ javaTester/eclipseTips/Topic01_Template.java
@@ -0,0 +1,16 @@
+package eclipseTips;
+
+public class Topic01_Template {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// WebDriver driver;
+
+		// WebElement field = driver.findElement(By.xpath(""));
+		//
+		// WebElement field = driver.findElement(By.cssSelector(""));
+
+	}
+
+}

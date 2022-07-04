232362f8ffdcc4d4509998326045aeebb9a17d95
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-07-04 21:36:43 +0700

    update code

--- javaTester/javaOOP/inheritance/Dog.java
+++ javaTester/javaOOP/inheritance/Dog.java
@@ -1,8 +1,21 @@
 package javaOOP.inheritance;
 
 public class Dog extends Animal {
+	private int age;
+
 	public void run() {
 		System.out.println("Running ...");
 	}
 
+	public void setAge(int age) {
+		if (age > 0) {
+			this.age = age;
+		} else {
+			this.age = 0;
+		}
+	}
+
+	public int getAge() {
+		return this.age;
+	}
 }
--- javaTester/javaOOP/inheritance/Test.java
+++ javaTester/javaOOP/inheritance/Test.java
@@ -3,10 +3,15 @@ package javaOOP.inheritance;
 public class Test {
 
 	public static void main(String[] args) {
-		HuskyDog dog = new HuskyDog();
-		dog.run();
-		dog.eat();
-		dog.hun();
+		Dog dog = new Dog();
+		dog.setAge(-15);
+		System.out.println(dog.getAge());
+
+		dog.setAge(15);
+		System.out.println(dog.getAge());
+
+		dog.setAge(0);
+		System.out.println(dog.getAge());
 	}
 
 }

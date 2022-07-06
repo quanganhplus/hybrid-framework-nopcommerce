dd70856542efae454424a74b484e06d011c5801a
quanganhplus <quanganh.plus@gmail.com>
2022-07-06 16:23:44 +0700

    Java OOP - Part IX: Java and Selenium Exception Type

--- javaTester/javaException/HandleException.java
+++ javaTester/javaException/HandleException.java
@@ -1,17 +1,42 @@
 package javaException;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HandleException {
+	WebDriver driver;
 
-	public static void main(String[] args) {
-		String[] browserName = { "Chrome", "FireFox", "Edge" };
-		System.out.println(browserName[0]);
-		System.out.println(browserName[1]);
-		System.out.println(browserName[2]);
+	// @Test
+	public void TC_01() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 
-		// System.out.println(browserName[3]);
+		driver.get("http://live.techpanda.org/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-		int number = 15 / 0;
-		System.out.println(number);
+		driver.findElement(By.cssSelector("//div[@div='header-account']//a[text()='My Account']")).click();
 	}
 
+	@Test
+	public void TC_02() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+
+		driver.get("https://automationfc.github.io/basic-form/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		driver.findElement(By.cssSelector("#disable_password")).sendKeys("quang anh");
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit();
+	}
 }

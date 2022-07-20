c8a728e4a6becc23a8df26522cd4da9c658a7c62
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-07-20 23:44:56 +0700

    commit update code - 11h45pm

--- RunAllure.bat
+++ RunAllure.bat
@@ -2,5 +2,5 @@ set ProjectPath=%~dp0
 cd %ProjectPath%
 echo %ProjectPath%
 set p=%PATH%
-java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.8.10.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllure\*;%ProjectPath%libAllureReport\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4j\*;%ProjectPath%libWebDriverManager5\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceUser.xml"
+java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.8.10.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllure\*;%ProjectPath%libAllureReport\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4j\*;%ProjectPath%libWebDriverManager\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceUser.xml"
 pause

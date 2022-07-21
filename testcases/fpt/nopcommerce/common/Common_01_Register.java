e53e6623973a2cd986313e337cdb6e85aaeb7647
quanganhplus <quanganh.plus@gmail.com>
2022-07-21 17:19:29 +0700

    Framework 29 - Share Login/ Cookie state

---
+++ testcases/fpt/nopcommerce/common/Common_01_Register.java
@@ -0,0 +1,65 @@
+package fpt.nopcommerce.common;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import commons.BaseTest;
+import commons.PageGeneratorManager;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserRegisterPageObject;
+
+public class Common_01_Register extends BaseTest {
+	private WebDriver driver;
+	private String firstName, lastName;
+	public static String emailAddress, password;
+	private UserHomePageObject homePage;
+	private UserRegisterPageObject registerPage;
+
+	@Parameters("browser")
+	@BeforeTest(description = "Create new common User for all Classes Test")
+	public void Register(String browserName) {
+		driver = getBrowserDriver(browserName);
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+
+		firstName = "quang anh";
+		lastName = "trinh";
+		emailAddress = "quanganh" + generateFakeNumber() + "@gmail.com";
+		password = "123456";
+
+		log.info("Register - Step 01: Navigate to 'Register' page");
+		registerPage = homePage.openRegisterPage();
+
+		log.info("Register - Step 02: Enter to Firstname textbox with value is '" + firstName + "'");
+		registerPage.inputToFistnameTextbox(firstName);
+
+		log.info("Register - Step 03: Enter to Lastname textbox with value is '" + lastName + "'");
+		registerPage.inputToLastNameTextbox(lastName);
+
+		log.info("Register - Step 04: Enter to Email textbox with value is '" + emailAddress + "'");
+		registerPage.inputToEmailTextbox(emailAddress);
+
+		log.info("Register - Step 05: Enter to Password textbox with value is '" + password + "'");
+		registerPage.inputToPasswordTextbox(password);
+
+		log.info("Register - Step 06: Enter to Confirm Password textbox with value is '" + password + "'");
+		registerPage.inputToConfirmPasswordTextbox(password);
+
+		log.info("Register - Step 07: Click to 'Register' button");
+		registerPage.clickToRegisterButton();
+
+		log.info("Register - Step 08: Verify Register success message is displayed");
+		verifyEquals(registerPage.getRegisterSucessMessage(), "Your registration completed");
+
+		log.info("Register - Step 09: Click to Logout link");
+		homePage = registerPage.clickToLogoutLink();
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
---
+++ testcases/fpt/nopcommerce/user/Level_16_Share_Data.java
@@ -0,0 +1,78 @@
+package fpt.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import commons.PageGeneratorManager;
+import fpt.nopcommerce.common.Common_01_Register;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserLoginPageObject;
+
+public class Level_16_Share_Data extends BaseTest {
+	private WebDriver driver;
+	private String emailAddress, validPassword;
+	private UserHomePageObject homePage;
+	private UserLoginPageObject loginPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+
+		emailAddress = Common_01_Register.emailAddress;
+		validPassword = Common_01_Register.password;
+
+		log.info("Login - Step 01: Navigate to Login page");
+		loginPage = homePage.openLoginPage();
+
+		log.info("Login - Step 02: Enter to Email textbox with value is '" + emailAddress + "'");
+		loginPage.inputToEmailTextbox(emailAddress);
+
+		log.info("Login - Step 03: Enter to Password textbox with value is '" + validPassword + "'");
+		loginPage.inputToPasswordTextbox(validPassword);
+
+		log.info("Login - Step 04: Click to 'Login' button");
+		homePage = loginPage.clickToLoginButton();
+	}
+
+	@Test
+	public void Search_01_Emty_Data() {
+
+	}
+
+	@Test
+	public void Search_02_Relative_Product_Name() {
+
+	}
+
+	@Test
+	public void Search_03_Absolute_Product_Name() {
+
+	}
+
+	@Test
+	public void Search_04_Parent_Category() {
+
+	}
+
+	@Test
+	public void Search_05_Incorrect_Manufactorer() {
+
+	}
+
+	@Test
+	public void Search_06_Correct_Manufactorer() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}

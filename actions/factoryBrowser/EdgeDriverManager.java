24a42e5b792afbd41956d4023ffc76a727c909d7
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-09-06 22:37:10 +0700

    commit - update code

--- actions/factoryBrowser/EdgeDriverManager.java
+++ actions/factoryBrowser/EdgeDriverManager.java
@@ -4,18 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
-import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC;;
+import io.github.bonigarcia.wdm.WebDriverManager;;
 
 public class EdgeDriverManager implements BrowserFactory {
 
 	@Override
 	public WebDriver getBrowserDriver() {
-		if (!IS_OS_WINDOWS || !IS_OS_MAC) {
-			throw new BrowserNotSupportedException("Edge is not supported on " + System.getProperty("os.name"));
-		}
 		// System.setProperty("webdriver.edge.driver", projectPath + ".//browserDrivers//msedgedriver.exe");
 		WebDriverManager.edgedriver().setup();
 		EdgeOptions options = new EdgeOptions();
